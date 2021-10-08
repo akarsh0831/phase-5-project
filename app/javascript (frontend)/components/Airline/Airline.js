b0b5dbf7c383ad22c8eda219ef9cb67f703f0c04
akarsh <akarshshukla19@yahoo.com>
2021-10-08 19:49:25 -0400

    mvp

---
+++ app/javascript (frontend)/components/Airline/Airline.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Header from './Header'
+
+const Airline = (props) => {
+    const [airline, setAirline] = useState({})
+    const [reviews, setReviews] = useState({})
+    const [loaded, setLoaded] = useState(false)
+
+    useEffect(()=> {
+        const slug = props.match.params.slug
+        const url = `/api/v1/airlines/${slug}`
+
+        axios.get(url)
+        .then( resp => {
+            setAirline(resp.data) 
+            setLoaded(true)
+        })
+        .catch( resp => console.log(resp) )
+    }, [])
+
+    return (
+        <div className="wrapper">
+            <div className="column">
+                {
+                    loaded &&
+                    <Header
+                        attributes={airline.data.attributes}
+                    />
+                }
+                <div className="reviews"></div>
+            </div>
+            <div className="column">
+                <div className="review-form">[Review form goes here.]</div>
+            </div>
+        </div>
+    )
+}
+
+export default Airline
